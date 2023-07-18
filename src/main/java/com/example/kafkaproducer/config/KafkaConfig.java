d6969e554fda7316311cbc3070817ef66723a032
newbe01 <pkwaa3@gmail.com>
2023-07-18 23:08:33 +0900

    no message

--- src/main/java/com/example/kafkaproducer/config/KafkaConfig.java
+++ src/main/java/com/example/kafkaproducer/config/KafkaConfig.java
@@ -1,14 +1,14 @@
 package com.example.kafkaproducer.config;
 
-import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -26,11 +26,31 @@ public class KafkaConfig {
     public ProducerFactory<String, Object> ProducerFactory() {
 
         Map<String, Object> config = new HashMap<>();
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "3.38.179.14:9092, 13.125.222.154:9092, 13.125.238.140:9092");
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "3.38.179.14:9092");
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 
         return new DefaultKafkaProducerFactory<>(config);
     }
+
+    @Bean
+    public ConsumerFactory<String, Object> ConsumerFactory() {
+
+        Map<String, Object> config = new HashMap<>();
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "3.38.179.14:9092");
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+
+        return new DefaultKafkaConsumerFactory<>(config);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(ConsumerFactory());
+
+        return factory;
+    }
+
 
 }
