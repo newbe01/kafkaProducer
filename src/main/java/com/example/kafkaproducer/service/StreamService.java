06973e3034fd5ae2472e9308326007d4e1ee9ae2
newbe01 <pkwaa3@gmail.com>
2023-07-20 21:32:44 +0900

    kafka - join

--- src/main/java/com/example/kafkaproducer/service/StreamService.java
+++ src/main/java/com/example/kafkaproducer/service/StreamService.java
@@ -4,9 +4,13 @@ import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.JoinWindows;
 import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Printed;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.Duration;
 
 @Service
 public class StreamService {
@@ -16,8 +20,30 @@ public class StreamService {
     @Autowired
     public void buildPipeline(StreamsBuilder streamsBuilder) {
 
-        KStream<String, String> myStream = streamsBuilder.stream("testTopic", Consumed.with(STRING_SERDE, STRING_SERDE));
-        myStream.filter((key, value) -> value.contains("test")).to("testList");
+//        KStream<String, String> myStream = streamsBuilder.stream("testTopic", Consumed.with(STRING_SERDE, STRING_SERDE));
+//        myStream.print(Printed.toSysOut());
+//        myStream.filter((key, value) -> value.contains("test")).to("testList");
+
+        KStream<String, String> leftStream = streamsBuilder.stream(
+                "leftTopic",
+                Consumed.with(STRING_SERDE, STRING_SERDE)
+        ).selectKey((k,v) -> v.substring(0,v.indexOf(":")));
+
+        KStream<String, String> rightStream = streamsBuilder.stream(
+                "rightTopic",
+                Consumed.with(STRING_SERDE, STRING_SERDE)
+        ).selectKey((k,v) -> v.substring(0,v.indexOf(":")));
+
+        leftStream.print(Printed.toSysOut());
+        rightStream.print(Printed.toSysOut());
+
+        KStream<String, String> joinStream = leftStream.join(rightStream,
+                (leftValue, rightValue) -> leftValue + "_" + rightValue,
+                JoinWindows.ofTimeDifferenceWithNoGrace(Duration.ofMinutes(1))
+        );
+
+     joinStream.print(Printed.toSysOut());
+     joinStream.to("joinedMsg");
 
     }
 
